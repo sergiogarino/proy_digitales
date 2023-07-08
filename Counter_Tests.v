534f3f6f2838c6984928f4da0b981b5d0572a658
sergiogarino <sgarino22@gmail.com>
2023-07-07 19:24:38 -0600

    Se añaden pruebas al contador

--- Counter_Tests.v
+++ Counter_Tests.v
@@ -7,7 +7,7 @@
  * archivo: asm_ascensor.v
  * autores:
  * Sergio Garino Vargas - B73157
- * Johan - 
+ * Johan Herrera Chaves - C03811
  *
  * Descripción: 
  * Test bench para el módulo Counter
@@ -28,17 +28,35 @@ module Counter_Tests(
 	    $dumpfile("Counter.vcd");
 	    $dumpvars;
 
-		//Prubas para el contador de 4 bits
+        //Prubas para el contador de 4 bits
 
-	    // Prueba 1 - Cuenta Ascendente 
+        //Prueba 1 - Cuenta Ascendente 
         modo    = 1;
-		#3 enb  = 1;
-		#1 data = 0;
-		#3 modo = 0;
-		#50 enb = 0;        	
-		
-		#10 $finish;
+        #3 enb  = 1;
+        #1 data = 0;
+        #3 modo = 0;
+        #30 enb = 0;          
+
+
+        // Prueba 2 - Deshabilitar en medio del conteo y luego volver a habilitar
+        //#30 modo = 1;
+        #3 enb   = 1;
+        #1 data  = 0;
+        //#3 modo  = 0;
+        #8 enb   = 0; // Deshabilitar el contador después de 5 ciclos de reloj
+        #10 enb  = 1; // Volver a habilitar el contador
+
+        // Prueba 3 - Iniciar el conteo en un valor dado utilizando la carga de datos paralela
+        modo    = 1;
+        #3 enb  = 1;
+        #1 data = 2; // Iniciar el conteo en el valor 2
+        #3 modo = 0;
+
+
+        #10 $finish;
 		
 	end 
 
 endmodule
+
+
